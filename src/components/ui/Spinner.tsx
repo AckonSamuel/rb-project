f21879625adcb37045259bb8adf7b33558b061e8
Samuel Ackon <ackons433@gmail.com>
2024-11-26 22:16:13 +0000

    shadcn ui components

---
+++ src/components/ui/Spinner.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+const Spinner: React.FC = () => (
+  <svg
+    version="1.1"
+    id="L9"
+    xmlns="http://www.w3.org/2000/svg"
+    xmlnsXlink="http://www.w3.org/1999/xlink"
+    viewBox="0 0 100 100"
+    enableBackground="new 0 0 100 100"
+    xmlSpace="preserve"
+    width="50"
+    height="50"
+  >
+    <circle
+      fill="none"
+      stroke="#000"
+      strokeWidth="4"
+      strokeLinecap="round"
+      cx="50"
+      cy="50"
+      r="40"
+      strokeDasharray="250"
+      strokeDashoffset="0"
+    >
+      <animate
+        attributeName="stroke-dashoffset"
+        values="0;502"
+        dur="1.5s"
+        keyTimes="0;1"
+        repeatCount="indefinite"
+      />
+    </circle>
+  </svg>
+);
+
+export default Spinner;
---
+++ src/components/ui/ToastWrapper.tsx
@@ -0,0 +1,9 @@
+// components/ui/ToastWrapper.tsx
+"use client";
+import { Toaster } from "react-hot-toast";
+
+const ToastWrapper = () => {
+  return <Toaster position="top-right" reverseOrder={false} />;
+};
+
+export default ToastWrapper;
