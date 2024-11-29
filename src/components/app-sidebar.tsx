8f3e63de94233698585f4041c9f3297ef64dfc73
Samuel Ackon <ackons433@gmail.com>
2024-11-29 01:04:56 +0000

    Fix linter issues

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -1,20 +1,16 @@
-import * as React from "react"
-import { IoIosMail } from "react-icons/io";
-import { FaBoxOpen } from "react-icons/fa";
-import { HiUserGroup } from "react-icons/hi2";
-import { IoBarChartSharp } from "react-icons/io5";
-import { IoGrid } from "react-icons/io5";
-import { FaWallet } from "react-icons/fa6";
-import { PiGearFineBold } from "react-icons/pi";
-import { BsShieldLockFill } from "react-icons/bs";
+/* eslint-disable @typescript-eslint/no-explicit-any */
 
-import {
-  Sidebar,
-  SidebarContent,
-  SidebarHeader,
-  SidebarRail,
-} from "@/components/ui/sidebar"
-import { useState } from "react";
+import React, {useState} from "react";
+import {IoIosMail} from "react-icons/io";
+import {FaBoxOpen} from "react-icons/fa";
+import {HiUserGroup} from "react-icons/hi2";
+import {IoBarChartSharp} from "react-icons/io5";
+import {IoGrid} from "react-icons/io5";
+import {FaWallet} from "react-icons/fa6";
+import {PiGearFineBold} from "react-icons/pi";
+import {BsShieldLockFill} from "react-icons/bs";
+
+import {Sidebar, SidebarContent, SidebarHeader, SidebarRail} from "@/components/ui/sidebar";
 
 // This is sample data.
 const data = [
@@ -68,31 +64,28 @@ const data = [
         title: "Security",
         url: "#",
         icon: <BsShieldLockFill size={25} />,
-      }
+      },
     ],
   },
 ];
 
-export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
-
+export function AppSidebar({...props}: React.ComponentProps<typeof Sidebar>) {
   const [navData, setNavData] = useState<any>(data);
 
   const handleNavigate = (parentIndex: number, childIndex: number) => {
-    setNavData((prevNavData: any[]) => 
+    setNavData((prevNavData: any[]) =>
       prevNavData.map((section, sectionIdx) => ({
         ...section,
         items: section.items.map((item: any, itemIdx: any) => ({
           ...item,
           isActive: sectionIdx === parentIndex && itemIdx === childIndex, // Activate only the clicked item
         })),
-      }))
+      })),
     );
   };
-  
-
 
   return (
-    <Sidebar {...props} className="bg-red-500" style={{ width: "200px" }}>
+    <Sidebar {...props} className="bg-red-500" style={{width: "200px"}}>
       <SidebarHeader>
         <h2 className="text-white text-center text-2xl mt-2 font-bold mb-8">RB</h2>
       </SidebarHeader>
@@ -103,8 +96,9 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
               <hr className="border-[#9e9a9a] w-[85%] mr-2 ml-auto mt-8 mb-2" />
             )}
             <div
-              className={`text-white text-md mb-4 ml-6 ${section.title === "General" ? "text-[#9e9a9a]" : "mb-8"
-                }`}
+              className={`text-white text-md mb-4 ml-6 ${
+                section.title === "General" ? "text-[#9e9a9a]" : "mb-8"
+              }`}
             >
               {section.title}
             </div>
@@ -112,23 +106,19 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
               {section.items.map((item: any, itemIdx: number) => (
                 <div
                   key={item.title}
-                  onClick={() => handleNavigate(sectionIdx, itemIdx)} // Pass parent and child indices
                   className="w-full cursor-pointer"
+                  onClick={() => handleNavigate(sectionIdx, itemIdx)} // Pass parent and child indices
                 >
                   <div className="text-sm h-8 mb-2 flex gap-2">
                     <div
-                      className={`h-8 w-1 rounded-lg mr-2 ${item.isActive ? "bg-[#9BF22B]" : ""
-                        }`}
-                    ></div>
-                    <div
-                      className={`${item.isActive ? "text-[#9BF22B]" : ""} my-auto`}
-                    >
+                      className={`h-8 w-1 rounded-lg mr-2 ${item.isActive ? "bg-[#9BF22B]" : ""}`}
+                    />
+                    <div className={`${item.isActive ? "text-[#9BF22B]" : ""} my-auto`}>
                       {item.icon}
                     </div>
                     <a
+                      className={`ml-2 my-auto ${item.isActive ? "text-white" : ""}`}
                       href={item.url}
-                      className={`ml-2 my-auto ${item.isActive ? "text-white" : ""
-                        }`}
                     >
                       {item.title}
                     </a>
@@ -140,7 +130,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
         ))}
         <hr className="border-[#9e9a9a] w-[85%] mr-2 ml-auto mt-8 -mb-6" />
         <div className="flex gap-2 mt-8 ml-4 -mr-1">
-          <div className="w-8 h-8 bg-[#9BF22B] mt-0 rounded-full"></div>
+          <div className="w-8 h-8 bg-[#9BF22B] mt-0 rounded-full" />
           <div>
             <p className="text-white text-xs -mb-1">Fandaww Punx</p>
             <small>fandaww6@gmail.com</small>
@@ -149,6 +139,5 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
       </SidebarContent>
       <SidebarRail />
     </Sidebar>
-
-  )
+  );
 }
