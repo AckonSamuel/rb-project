f12974e3bcd6341403b432b1f60dc4c3510599ba
Samuel Ackon <ackons433@gmail.com>
2024-11-29 00:38:28 +0000

    refactor upgrade card

--- src/components/dashboard/UpgradeCard.tsx
+++ src/components/dashboard/UpgradeCard.tsx
@@ -3,13 +3,13 @@ import { Card, CardContent } from "../ui/card";
 
 const UpgradeCard = () => {
     return (
-        <Card className="col-span-1 lg:col-span-3 bg-bud">
+        <Card className="col-span-1 lg:col-span-3 bg-[#CDC7BA]">
         <CardContent className="flex flex-col items-center justify-between">
           <div className="mt-12">
             <p className="text-md font-semibold">Level up your sales management to the next level</p>
             <p className="text-gray-600 text-xs">An any way to manage sales with care and precision</p>
           </div>
-          <Button color="success" className="mt-4 w-full text-white bg-green-800">
+          <Button color="success" className="mt-4 w-full text-white bg-[#296C30]">
             Upgrate to Siohioma+
           </Button>
         </CardContent>
